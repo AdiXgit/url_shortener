dd007d7b8fe5590389a78e775497a575585ccd7d
AdiXgit <pes2ug23cs031@pesu.pes.edu>
2025-03-19 23:17:46 +0530

    edited client and server code

--- src/client.c
+++ src/client.c
@@ -1,32 +1,65 @@
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #pragma comment(lib, "ws2_32.lib")  // Link Winsock library
+#else
+    #include <unistd.h>
+    #include <arpa/inet.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 
 #define PORT 8080
-#define buffer_size 2048
+#define BUFFER_SIZE 2048
 
-int main(){
-    int sock = socket(AF_INET,SOCK_STREAM,0);
+#ifdef _WIN32
+    typedef SOCKET socket_t;
+    #define close_socket closesocket
+#else
+    typedef int socket_t;
+    #define close_socket close
+#endif
+
+int main() {
+    #ifdef _WIN32
+        WSADATA wsa;
+        WSAStartup(MAKEWORD(2,2), &wsa);
+    #endif
+
+    socket_t sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) {
+        perror("Socket creation failed");
+        return 1;
+    }
+
     struct sockaddr_in server_addr;
-
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
     server_addr.sin_port = htons(PORT);
 
-    connect(sock,(struct sockaddr*)&server_addr,sizeof(server_addr));
-    char request[buffer_size],response[buffer_size];
+    if (connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Connection failed");
+        return 1;
+    }
 
-    printf("Enter command(SHORTEN <url> / LOOKUP <url>): ");
-    fgets(request,buffer_size,stdin);
-    request[strcspn(request,"\n")] = 0;
+    char request[BUFFER_SIZE], response[BUFFER_SIZE];
 
-    send(sock,request,strlen(request),0);
-    read(sock,response,buffer_size);
-    printf("%s\n",response);
-    close(sock);
+    printf("Enter command (SHORTEN <url> / EXPAND <short_url>): ");
+    fgets(request, BUFFER_SIZE, stdin);
+    request[strcspn(request, "\n")] = 0;  // Remove newline
+
+    send(sock, request, strlen(request), 0);
+    recv(sock, response, BUFFER_SIZE, 0);
+
+    printf("%s\n", response);
+
+    close_socket(sock);
+
+    #ifdef _WIN32
+        WSACleanup();
+    #endif
+
     return 0;
 }
-
-
--- src/server.c
+++ src/server.c
@@ -1,59 +1,109 @@
+#ifdef _WIN32
+    #include <winsock2.h>
+    #include <ws2tcpip.h>
+    #include <windows.h>
+    #pragma comment(lib, "ws2_32.lib")  // Link Winsock library
+#else
+    #include <unistd.h>
+    #include <arpa/inet.h>
+    #include <pthread.h>
+#endif
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <pthread.h>
 #include "url_shortener.h"
 
 #define PORT 8080
 #define BUFFER_SIZE 2048
-void* handle_client(void* socket_desc){
-    int sock  = *(int*)socket_dec;
+
+#ifdef _WIN32
+    typedef SOCKET socket_t;
+    #define close_socket closesocket
+#else
+    typedef int socket_t;
+    #define close_socket close
+#endif
+
+void* handle_client(void* socket_desc) {
+    socket_t sock = *(socket_t*)socket_desc;
+    free(socket_desc);
+
     char buffer[BUFFER_SIZE] = {0};
+    char command[16], argument[BUFFER_SIZE], response[BUFFER_SIZE];
 
-    read(sock,buffer,BUFFER_SIZE);
-    sscanf(buffer,"%s %s",command,argument);
+    recv(sock, buffer, BUFFER_SIZE, 0);
+    sscanf(buffer, "%s %s", command, argument);
 
-    if(strcmp(command,"SHORTEN") == 0){
-        char*short_url = shorten_url(argument);
-        sprintf(response,"SHORTENDED %s\n",short_url);
+    if (strcmp(command, "SHORTEN") == 0) {
+        char* short_url = shorten_url(argument);
+        sprintf(response, "SHORTENED %s\n", short_url);
         free(short_url);
-    } else if(strcmp(command,"EXPAND") == 0){
+    } else if (strcmp(command, "EXPAND") == 0) {
         char* long_url = expand_url(argument);
-        sprintf(response,"EXPANDED %s\n",long_url);
+        sprintf(response, "EXPANDED %s\n", long_url);
         free(long_url);
     } else {
-        sprintf(response,"ERROR\n");
+        strcpy(response, "ERROR\n");
     }
-    write(sock,response,strlen(response));
-    close(sock);
-    free(socket_desc);
+
+    send(sock, response, strlen(response), 0);
+    close_socket(sock);
     return NULL;
 }
 
-int main(){
-    int server_fd,client_sock;
-    struct sockaddr_in server_addr,client_addr;
-    socket_t client_len = sizeof(client_addr);
+int main() {
+    #ifdef _WIN32
+        WSADATA wsa;
+        WSAStartup(MAKEWORD(2,2), &wsa);
+    #endif
 
-    server_fd = socket(AF_INET,SOCK_STREAM,0);
+    socket_t server_fd, client_sock;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len = sizeof(client_addr);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("Socket failed");
+        return 1;
+    }
+
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    bind(server_fd,(struct sockaddr*)&server_addr,sizeof(server_addr));
-    listen(server_fd,5);
-    printf("Server listening on port %d\n",PORT);
-
-    while(1){
-        client_sock = accept(server_fd,(struct sockaddr*)&client_addr,&client_len);
-        pthread_t thread;
-        int* socket_desc = (int*)malloc(sizeof(int));
-        *socket_desc = client_sock;
-        pthread_create(&thread,NULL,handle_client,(void*)socket_desc);
-        pthread_detach(thread);
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Bind failed");
+        return 1;
     }
+
+    listen(server_fd, 5);
+    printf("Server listening on port %d\n", PORT);
+
+    while (1) {
+        client_sock = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+        if (client_sock < 0) {
+            perror("Accept failed");
+            continue;
+        }
+
+        #ifdef _WIN32
+            HANDLE thread;
+            thread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)handle_client, (void*)&client_sock, 0, NULL);
+            CloseHandle(thread);
+        #else
+            pthread_t thread;
+            socket_t* socket_desc = (socket_t*)malloc(sizeof(socket_t));
+            *socket_desc = client_sock;
+            pthread_create(&thread, NULL, handle_client, (void*)socket_desc);
+            pthread_detach(thread);
+        #endif
+    }
+
+    close_socket(server_fd);
+    #ifdef _WIN32
+        WSACleanup();
+    #endif
+
     return 0;
-    
 }
